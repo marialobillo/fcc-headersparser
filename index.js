b04abf60240828e1db0fcb4e16a99e7f4463ab8c
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-07-11 13:10:01 +0200

    chore: basic dev setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 // where your node app starts
-
+require('dotenv').config();
 // init project
 require('dotenv').config();
 var express = require('express');
@@ -23,6 +23,15 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// API endpoint
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress, language, software });
+});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
